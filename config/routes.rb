c0f925864f450be593ad3ff0f7f35eca0311eb4e
Rob <epr1935@icloud.com>
2017-03-03 17:54:43 -0500

    changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :posts
   resources :users
-  post '/signup' => 'users#create'
-  post '/signin' => 'users#signin'
-  post '/post'   => 'posts#create'
+  post '/signup'   => 'users#create'
+  post '/signin'   => 'users#signin'
+  post '/post'     => 'posts#create'
+  get  '/user'     => 'users#index'
+  get  '/timeline' => 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
