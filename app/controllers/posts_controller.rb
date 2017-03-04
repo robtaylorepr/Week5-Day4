892ac662aed27eb331bda0f76f3e3e58aee0dc8b
Rob <epr1935@icloud.com>
2017-03-04 14:12:06 -0500

    added require cors in gemfile

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :require_user, only: [:create]
 
   def index
     @posts = Post.all
@@ -7,17 +8,23 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    if @post.save
-      render json: @post
+    if current_user
+      binding.pry
+      @post.user = current_user
+      if @post.save
+        render json: @post
+      else
+        render json: @post.errors.full_messages, status: 400
+      end
     else
-      render json: @post.errors.full_messages, status: 400
+      require_user
     end
   end
 
   private
 
   def post_params
-    params.permit(:body, :api_token)
+    params.permit(:body)
   end
 
 end
