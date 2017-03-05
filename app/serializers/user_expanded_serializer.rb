152421891e06746aa8a4d1d2e6d51b7d2e262fbe
Rob <epr1935@icloud.com>
2017-03-05 14:34:16 -0500

    changed name of follow methed in UserExpandedSerializer

--- app/serializers/user_expanded_serializer.rb
+++ app/serializers/user_expanded_serializer.rb
@@ -1,7 +1,7 @@
 class UserExpandedSerializer < ActiveModel::Serializer
-  attributes :name, :uname, :id, :photo, :api_token, :follow
+  attributes :name, :uname, :id, :photo, :api_token, :followstatus
 
-  def follow
+  def followstatus
     if current_user
       current_user.follows?(object)
     else
